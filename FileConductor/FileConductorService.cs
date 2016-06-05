a089598b48f90e34214fba00781900ba68c8a67a
iks111 <dawidsibinski@gmail.com>
2016-06-05 14:38:02 +0200

    Solution skeleton added

---
+++ FileConductor/FileConductorService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileConductor
+{
+    class FileConductorService
+    {
+        public bool Start()
+        {
+            // Service's initialization logic
+            // Executed when the service starts
+            return true;
+        }
+
+        public bool Stop()
+        {
+            // Service's disposing logic
+            // Executed when the service is stopped/closed
+            return true;
+        }
+    }
+}
