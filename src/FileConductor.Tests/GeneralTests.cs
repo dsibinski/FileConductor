cba74f2bc77cbc264b3d51ddc0cfff7493cc7d46
iks111 <dawidsibinski@gmail.com>
2016-06-11 21:30:28 +0200

    Test project created, NUnit added

---
+++ src/FileConductor.Tests/GeneralTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace FileConductor.Tests
+{
+    [TestFixture]
+    public class GeneralTests
+    {
+        // Sample test class
+        [Test]
+        public void SampleTest()
+        {
+            Assert.AreEqual(1,1);
+        }
+    }
+}
