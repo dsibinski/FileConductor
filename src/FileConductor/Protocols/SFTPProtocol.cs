a5883a05361fec69493a08a25875d9b6861425ad
iks111 <dawidsibinski@gmail.com>
2016-07-27 23:12:28 +0200

    Added empty SFTPProtocol.cs for compatibility

---
+++ src/FileConductor/Protocols/SFTPProtocol.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Renci.SshNet;
+
+namespace FileConductor.Protocols
+{
+    /*public class SFTPProtocol : Protocol
+    {
+        public override void Execute()
+        {
+            TryToMove();
+        }
+
+        private void TryToMove()
+        {
+            var sourcePath = Properties.SourceTarget.Path;
+            var destinyPath = Properties.DestinationTarget.Path;
+            var regex = Properties.Regex;
+
+            // SFTP data
+            //TODO: protocol should not be executing any tasks
+            //TODO: rething / redesign the concept of SFTP
+           // var user = Properties.Ser
+
+            using (var sftp = new SftpClient("host", "username", "password"))
+            {
+                sftp.Connect();
+                //var file = sftp.
+
+            }
+        }
+    }*/
+}
