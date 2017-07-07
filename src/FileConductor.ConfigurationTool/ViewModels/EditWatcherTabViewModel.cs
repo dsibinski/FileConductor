9a8e50ffaace0f9fdec03334e6536f3af4086bc9
Wojciech Grzesiak <wgrzesiak147@gmail.com>
2017-07-07 13:37:28 +0200

    Config moved back to FileConductor project + added procedute configuration in UI

--- src/FileConductor.ConfigurationTool/ViewModels/EditWatcherTabViewModel.cs
+++ src/FileConductor.ConfigurationTool/ViewModels/EditWatcherTabViewModel.cs
@@ -12,13 +12,40 @@ namespace ConfigurationTool.ViewModels
 {
    public class EditWatcherTabViewModel : Tab
    {
+       
+
+       public bool IsProcedureTriggered
+       {
+           get { return Watcher.ProcedureData != null; }
+           set
+           {
+               if (value == false)
+               {
+                    Watcher.ProcedureData = null;
+                   Watcher.WatcherData.DatabaseId = 0;
+               }
+               else
+               {
+                   if (Configuration.Databases.Any())
+                   {
+                       Watcher.ProcedureData = Configuration.Databases.FirstOrDefault();
+                   }
+                   else
+                   {
+                       throw new NotImplementedException(
+                           "No procedures defined in configuration file. This functionality is not implemented yet");
+                   }
+               }
+           }
+       }
+
        public event Action OnOperationModified;
        public Watcher Watcher { get; set; }
        public ConfigurationData Configuration { get; set; }
 
        public EditWatcherTabViewModel(ITabController controller, ConfigurationData config, Watcher watcher) : base(controller)
         {
-            Name = "Edit";
+            Name = watcher.WatcherData.Code;
             SaveCommand = new CommandHandler(SaveWatcher);
             Watcher = watcher;
             Configuration = config;
