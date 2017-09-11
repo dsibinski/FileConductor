c8fd5066e12f7ec26d46a3c185d6897a46f93393
Wojciech Grzesiak <wojciech.grzesiak@intive.com>
2017-09-11 11:47:02 +0200

    ServerEditView update

--- src/FileConductor.ConfigurationTool/Controls/CommonField.xaml.cs
+++ src/FileConductor.ConfigurationTool/Controls/CommonField.xaml.cs
@@ -1,31 +1,61 @@
-﻿using System;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
 
 namespace ConfigurationTool.Controls
 {
     /// <summary>
-    /// Interaction logic for CommonField.xaml
+    ///     Interaction logic for CommonField.xaml
     /// </summary>
     public partial class CommonField : UserControl
     {
+        public static readonly DependencyProperty TextProperty = DependencyProperty.Register("Text", typeof(string),
+            typeof(CommonField),
+            new FrameworkPropertyMetadata(string.Empty, FrameworkPropertyMetadataOptions.BindsTwoWayByDefault));
 
-        public static readonly DependencyProperty TextProperty = DependencyProperty.Register("Text", typeof(String),typeof(CommonField), new FrameworkPropertyMetadata(string.Empty, FrameworkPropertyMetadataOptions.BindsTwoWayByDefault));
-        public static readonly DependencyProperty ValueProperty = DependencyProperty.Register("Value", typeof(String), typeof(CommonField), new FrameworkPropertyMetadata(string.Empty, FrameworkPropertyMetadataOptions.BindsTwoWayByDefault));
-        public String Text
-        {
-            get { return GetValue(TextProperty).ToString(); }
-            set { SetValue(TextProperty, value); }
-        }
+        public static readonly DependencyProperty TextBoxWidthProperty = DependencyProperty.Register("TextBoxWidth",
+            typeof(double),
+            typeof(CommonField),
+            new FrameworkPropertyMetadata(100.00, FrameworkPropertyMetadataOptions.BindsTwoWayByDefault));
 
-        public String Value
-        {
-            get { return GetValue(ValueProperty).ToString(); }
-            set { SetValue(ValueProperty, value); }
-        }
+        public static readonly DependencyProperty ValueProperty = DependencyProperty.Register("Value", typeof(string),
+            typeof(CommonField),
+            new FrameworkPropertyMetadata(string.Empty, FrameworkPropertyMetadataOptions.BindsTwoWayByDefault));
+
+        public static readonly DependencyProperty NumberOfCharactersProperty =
+            DependencyProperty.Register("NumberOfCharacters", typeof(int), typeof(CommonField),
+                new FrameworkPropertyMetadata(1000, FrameworkPropertyMetadataOptions.BindsTwoWayByDefault));
+
         public CommonField()
         {
             InitializeComponent();
+        }
+
+        public double TextBoxWidth
+        {
+            get => (int) GetValue(TextBoxWidthProperty);
+            set => SetValue(TextBoxWidthProperty, value);
+        }
+
+        public int NumberOfCharacters
+        {
+            get => (int) GetValue(NumberOfCharactersProperty);
+            set => SetValue(NumberOfCharactersProperty, value);
+        }
+
+        public string Text
+        {
+            get => GetValue(TextProperty).ToString();
+            set
+            {
+                SetValue(TextProperty, value);
+                TextBox.Width = 20;
+            }
+        }
+
+        public string Value
+        {
+            get => GetValue(ValueProperty).ToString();
+            set => SetValue(ValueProperty, value);
         }
     }
 }
